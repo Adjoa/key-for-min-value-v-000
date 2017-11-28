d23242df3747edbfb49ec5fe37c916d001b83bd0
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-28 13:24:11 +0000

    automatically backed up by learn

---
+++ key_for_min.rb
@@ -0,0 +1,13 @@
+# prereqs: iterators, hashes, conditional logic
+# Given a hash with numeric values, return the key for the smallest value
+
+def key_for_min_value(name_hash)
+  if name_hash == {}
+    return nil
+  else
+    key_array = []
+    value_array = []
+    name_hash.each do |name, value|
+      
+  end
+end
